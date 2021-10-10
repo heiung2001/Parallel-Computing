456fc31bcfe34156aeaa1f71fbfa9d3de8fb35f9
heiung2001 <hieu.working2001@gmail.com>
2021-10-10 23:16:07 +0700

    modify pi program

--- Introduction to OpenMP/PI_cal.c
+++ Introduction to OpenMP/PI_cal.c
@@ -2,6 +2,7 @@
 #include <omp.h>
 
 #define NUM_THREADS 2
+#define PAD 128
 
 static long num_steps = 1000000000;
 double step;
@@ -9,7 +10,8 @@ double step;
 int main()
 {
     int i, nthreads;
-    double pi, sum[NUM_THREADS], total;
+    static double sum[NUM_THREADS][PAD];
+    double pi, total;
     double start, end;
 
     step = 1.0/(double) num_steps;
@@ -18,6 +20,7 @@ int main()
     start = omp_get_wtime();
     #pragma omp parallel
     {
+        int i;
         int ID = omp_get_thread_num();
         int nthrds = omp_get_num_threads();
         double x;
@@ -25,17 +28,17 @@ int main()
         // check how many threads did the environment actually give.
         if (ID == 0) nthreads = nthrds;
 
-        for (i = ID, sum[ID] = 0.0; i < num_steps; i = i + nthrds)
+        for (i = ID, sum[ID][0] = 0.0; i < num_steps; i = i + nthrds)
         {
             x = (i+0.5)*step;
-            sum[ID] += 4.0/(1.0+x*x);
+            sum[ID][0] += 4.0/(1.0+x*x);
         }
     }
     end = omp_get_wtime() - start;
 
     for (i = 0; i < nthreads; i++)
     {
-        total += sum[i];
+        total += sum[i][0];
     }
     pi = total*step;
     printf("pi is: %lf with %d threads in %lf sec", pi, nthreads, end);
