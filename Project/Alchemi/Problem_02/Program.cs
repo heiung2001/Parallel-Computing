14883ca799d808f92e2dcbeee95e3b8ac6848942
heiung2001 <hieu.working2001@gmail.com>
2022-01-01 15:21:40 +0700

    least common multiple

--- Project/Alchemi/Problem_02/Program.cs
+++ Project/Alchemi/Problem_02/Program.cs
@@ -6,14 +6,15 @@ using Alchemi.Core.Owner;
 
 namespace Problem_02
 {
-    class MultiMatrix : GApplication
+    class Program : GApplication
     {
-        public static GApplication App = new GApplication();
-        public static List<List<int>> Matrix = new List<List<int>>();
-        private static DateTime startTime;
+        static public GApplication App       = new GApplication();
+        static public List<List<int>> Matrix = new List<List<int>>();
+        static public DateTime startTime;
 
-        public static void DisplayMatrix()
+        static public void DisplayMatrix()
         {
+            // Console.WriteLine("Do you wanna print out the matrix? (Y/N): ");
             for (int i = 0; i < Matrix.Count; i++)
             {
                 for (int j = 0; j < Matrix[0].Count; j++)
@@ -24,15 +25,19 @@ namespace Problem_02
             }
         }
 
-        public static void ProcessInitial(out int m, out int n, out int k)
+        [STAThread]
+        static public void Main(string[] args)
         {
+            Console.WriteLine("[Least Common Multiple - Grid Application]");
+            Console.WriteLine("--------------------------------------------");
+
             Random rnd = new Random();
-            Console.Write("Input Number of Rows: ");
-            m = int.Parse(Console.ReadLine());
-            Console.Write($"Input Number of Cols: ");
-            n = int.Parse(Console.ReadLine());
-            Console.Write($"Define Number of Rows in Each Thread: ");
-            k = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of Rows: ");
+            var m = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the number of Cols: ");
+            var n = Int32.Parse(Console.ReadLine());
+            Console.Write("Define the number of Rows in each thread: ");
+            var k = Int32.Parse(Console.ReadLine());
 
             for (int i = 0; i < m; i++)
             {
@@ -44,105 +49,69 @@ namespace Problem_02
                 Matrix.Add(temp);
             }
             // DisplayMatrix();
-        }
 
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            int m, n, k;
-            Console.WriteLine("[Solution for Problem 2, Alchemi]\n--------------------------------\n");
-            Console.WriteLine("Press <enter> to start ...");
-            Console.ReadLine();
-
-            ProcessInitial(out m, out n, out k);
-            DisplayMatrix();
-
-            //for (int i = 0; i < m / k + 1; i++)
-            //{
-            //    if ((i + 1) * k <= m && i * k < m)
-            //    {
-            //        App.Threads.Add(new LCMcalculator(Matrix, i * k, i * k + k - 1));
-            //    }
-            //    else if (i * k <= m)
-            //    {
-            //        App.Threads.Add(new LCMcalculator(Matrix, i * k, m - 1));
-            //        Console.WriteLine(i * k);
-            //    }
-            //}
-
-            var cluster = m / k + 1;
-            for (int i = 0; i < cluster; i++)
+            var clusters = m / k;
+            for (int i = 0; i < clusters; i++)
             {
-                if (i == cluster - 1)
+                if (i == clusters - 1)
                 {
-                    App.Threads.Add(new LCMcalculator(Matrix, i * k, m - 1));
+                    App.Threads.Add(new LCMCalculator(Matrix, i * k, m - 1));
                 }
                 else
                 {
-                    App.Threads.Add(new LCMcalculator(Matrix, i * k, i * k + k - 1));
+                    App.Threads.Add(new LCMCalculator(Matrix, i * k, i * k + k - 1));
                 }
             }
 
-
-            GConnection gc = GConnection.FromConsole("localhost", "9000", "user", "user");
-
-            startTime = DateTime.Now;
-            //App = new GApplication(gc);
-            App.Connection = gc;
-            App.ApplicationName = "LCM Calculator - Alchemi";
-            App.Manifest.Add(new ModuleDependency(typeof(LCMcalculator).Module));
-            App.ThreadFinish += App_ThreadFinish;
+            Console.WriteLine("\n<Login the Alchemi Grid>");
+            GConnection gconn   = GConnection.FromConsole("localhost", "9000", "user", "user");
+            App.ApplicationName = "Least Common Multiple - Alchemi";
+            App.Connection      = gconn;
+            
+            App.Manifest.Add(new ModuleDependency(typeof(LCMCalculator).Module));
+            App.ThreadFinish      += App_ThreadFinish;
             App.ApplicationFinish += new GApplicationFinish(App_ApplicationFinish);
+
             startTime = DateTime.Now;
-            Console.WriteLine("Thread started");
+            Console.WriteLine("<Thread Started!>");
             App.Start();
             Console.ReadLine();
-
-            //App.Connection = new GConnection("localhost", 9000, "user", "user");
-            //App.Manifest.Add(new ModuleDependency(typeof(LCMcalculator).Module));
-            //App.ThreadFinish += App_ThreadFinish;
-            //App.ApplicationName = "BCNN";
-            //App.ApplicationFinish += new GApplicationFinish(App_ApplicationFinish);
-            //startTime = DateTime.Now;
-            //Console.WriteLine("Thread Started");
-            //App.Start();
-            //Console.ReadLine();
         }
 
-        private static void App_ThreadFinish(GThread thread)
+        static private void App_ThreadFinish(GThread thread)
         {
-            LCMcalculator s = thread as LCMcalculator;
-            Console.Write("Luong {0} ({1}:{2}):", thread.Id, s.start, s.end);
-            for (int i = 0; i < s.ans.Count; i++)
+            LCMCalculator work = thread as LCMCalculator;
+            Console.Write("Thread {0} ({1}:{2}): ", thread.Id, work.start, work.end);
+            for (int i = 0; i < work.results.Count; i++)
             {
-                Console.Write(s.ans[i] + " ");
+                Console.Write(work.results[i] + " ");
             }
-            Console.Write("\n");
+            Console.WriteLine("\n");
         }
 
-        private static void App_ApplicationFinish()
+        static private void App_ApplicationFinish()
         {
-            Console.WriteLine("Calculation finished after {0} seconds", DateTime.Now - startTime);
+            Console.WriteLine("Calculation finished after {0} second", DateTime.Now - startTime);
         }
     }
 
     [Serializable]
-    class LCMcalculator : GThread
+    class LCMCalculator : GThread
     {
         public int start, end;
         public List<List<int>> Matrix;
-        public List<int> ans = new List<int>();
+        public List<int> results = new List<int>();
 
-        public LCMcalculator(List<List<int>> Matrix, int start, int end)
+        public LCMCalculator(List<List<int>> Matrix, int start, int end)
         {
             this.Matrix = Matrix;
-            this.start = start;
-            this.end = end;
+            this.start  = start;
+            this.end    = end;
         }
 
-        public static int LCM(int a, int b)
+        static public int LCM(int a, int b)
         {
-            int result = a * b;
+            int res = a * b;
             while (a != 0 && b != 0)
             {
                 if (a > b)
@@ -150,7 +119,7 @@ namespace Problem_02
                 else
                     b %= a;
             }
-            return a > 0 ? result / a : result / b;
+            return a > 0 ? res / a : res / b;
         }
 
         public override void Start()
@@ -162,7 +131,7 @@ namespace Problem_02
                 {
                     lcm = LCM(lcm, Matrix[i][j]);
                 }
-                ans.Add(lcm);
+                results.Add(lcm);
             }
         }
     }
